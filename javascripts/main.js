43fe97f1b1f89a61c5590d1de83d9d64ece1a539
Victor Delgado <vdel26@gmail.com>
2013-05-07 11:45:10 +0200

    sticky navigation and styles

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-$(document).foundation();
+$(document).ready(function () {
+    // initialize foundation
+    $(document).foundation();
+
+    // sticky navigation in desktop
+    var stickyHeight = $('.top-bar').offset().top;
+    $(window).scroll(function(){
+        if ($('html').hasClass('no-touch')) {
+            if($(window).scrollTop() > stickyHeight) {
+                $('.top-bar').addClass('stickybar');
+                $('header').addClass('stickybar');
+            }
+            else {
+                $('.top-bar').removeClass('stickybar');
+                $('header').removeClass('stickybar');
+            }
+        }
+    });
+});
